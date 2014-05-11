707e218020e2d762551576659a6bf76eab5584ee
rajlaxmaniitg@gmail.com <rajlaxmaniitg@gmail.com@97675271-08a5-79cf-e857-e8fd9bc6f39e>
2014-05-11 14:57:54 +0000

    +Adding Junit test cases

---  blackjack-repo/BlackJackGame/src/com/blackjack/Junit/CardJunitTest.java
+++  blackjack-repo/BlackJackGame/src/com/blackjack/Junit/CardJunitTest.java
@@ -1,4 +1,4 @@
-package com.blackjack.Junit;
+package com.blackjack.JUnitTest;
 
 import static org.junit.Assert.*;
 
@@ -6,53 +6,50 @@ import org.junit.Test;
 
 import com.blackjack.bean.Card;
 
-public class CardJunitTest {
+public class CardJUnitTest {
+
+	@Test
+	public void testgetColorName() {
+		Card card = new Card(3, 4);
+		assertEquals("Black", card.getColorName());
+	}
 
 	@Test
 	public void testgetSuit() {
-		Card card=new Card(2, 4);
+		Card card = new Card(2, 4);
 		assertEquals(2, card.getSuit());
 	}
-	
-	@Test
-	public void testgetColorName() {
-		Card card=new Card(3, 4);
-		assertEquals("Black", card.getColorName());
-	}
-	
+
 	@Test
 	public void testgetValue() {
-		Card card=new Card(3, 4);
+		Card card = new Card(3, 4);
 		assertEquals(4, card.getValue());
 	}
-	
-	
+
 	@Test
-	public void testgetHighValue(){
-		Card card=new Card(3, 4);
+	public void testgetHighValue() {
+		Card card = new Card(3, 4);
 		assertEquals(4, card.getHighValue());
 	}
-	
+
 	@Test
-	public void testgetlowValue(){
-		Card card=new Card(3, 4);
-		
+	public void testgetlowValue() {
+		Card card = new Card(3, 4);
+
 		assertEquals(4, card.getLowValue());
 	}
 
+	@Test
+	public void testgetFaceName() {
+		Card card = new Card(3, 4);
+
+		assertEquals("Four", card.getFaceName());
+	}
 
 	@Test
-	public void testgetFaceName(){
-		Card card=new Card(3, 4);
-		
-		assertEquals("Five", card.getFaceName());
-	}
-	
-	@Test
-	public void testgetSuitName(){
-		Card card=new Card(3, 4);
-		
+	public void testgetSuitName() {
+		Card card = new Card(3, 4);
+
 		assertEquals("Spades", card.getSuitName());
 	}
-
 }
---  blackjack-repo/BlackJackGame/src/com/blackjack/Junit/PlayerPanelTest.java
+++  blackjack-repo/BlackJackGame/src/com/blackjack/Junit/PlayerPanelTest.java
@@ -1,4 +1,4 @@
-package com.blackjack.Junit;
+package com.blackjack.JUnitTest;
 
 import static org.junit.Assert.*;
 
@@ -9,15 +9,24 @@ import org.junit.Test;
 import com.blackjack.GUI.PlayerPanel;
 import com.blackjack.bean.Card;
 
-
-
 public class PlayerPanelTest {
 
 	@Test
-	public void testaskBet(){
-		Image i =null;
-		PlayerPanel p=new PlayerPanel("player", true, 10, 5,i );		
+	public void testaskBet() {
+		Image i = null;
+		PlayerPanel p = new PlayerPanel("player", true, 1000, 5, i);
+
 		assertEquals(5, p.askBet(5));
-	}	
-	
+	}
+
+	@Test
+	public void testgetCurrentBet() {
+		Image i = null;
+		PlayerPanel p = new PlayerPanel("Player", true, 1000, 5, i);
+
+		Card card = new Card(3, 4);
+
+		assertEquals(0, p.getCurrentBet());
+	}
+
 }
